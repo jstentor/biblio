6a76893d2b9a6205db0a5500b13b19b6574cae6f
jstentor <jstentor@yahoo.es>
2018-06-15 20:54:14 +0200

    Se comentan las líneas de Auth para evitar la protección durante el desarrollo

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,7 +43,7 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
-        $this->loadComponent('Auth', [
+        /*$this->loadComponent('Auth', [
             'authenticate' => [
                 'Form' => [
                     'fields' => [
@@ -58,11 +58,11 @@ class AppController extends Controller
             ],
              // If unauthorized, return them to page they were just on
             'unauthorizedRedirect' => $this->referer()
-        ]);
+        ]);*/
 
         // Allow the display action so our PagesController
         // continues to work. Also enable the read only actions.
-        $this->Auth->allow(['display', 'view', 'index']);
+        //$this->Auth->allow(['display', 'view', 'index']);
 
         /*
          * Enable the following components for recommended CakePHP security settings.
@@ -72,6 +72,6 @@ class AppController extends Controller
         //$this->loadComponent('Csrf');
     }
     public function beforeRender(Event $event) {
-        $this->set('logged_user', $this->Auth->user('user'));
+        //$this->set('logged_user', $this->Auth->user('user'));
     }
 }
