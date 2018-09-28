9d130d5c004a79c835671ed751ea77d5a20d3e74
jstentor <jstentor@yahoo.es>
2018-09-28 12:46:39 +0200

    Modificacióin de validaciones de AutoresTable

--- src/Model/Table/AutoresTable.php
+++ src/Model/Table/AutoresTable.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Model\Table;
 
+use Cake\Event\Event;
+use Cake\Log\Log;
 use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
+use Cake\ORM\Entity;
 use Cake\Validation\Validator;
 
 /**
@@ -46,6 +49,8 @@ class AutoresTable extends Table
             'targetForeignKey' => 'libro_id',
             'joinTable' => 'autores_libros'
         ]);
+
+        $this->addBehavior('Search.Search');        
     }
 
     /**
@@ -63,20 +68,20 @@ class AutoresTable extends Table
         $validator
         ->scalar('ape_nom')
         ->maxLength('ape_nom', 150)
-        ->requirePresence('ape_nom', 'create')
-        ->notEmpty('ape_nom');
+        /*->requirePresence('ape_nom', 'create')*/
+        /*->notEmpty('ape_nom')*/;
 
         $validator
         ->scalar('nombre')
         ->maxLength('nombre', 100)
         ->requirePresence('nombre', 'create')
-        ->notEmpty('nombre');
+        /*->notEmpty('nombre')*/;
 
         $validator
         ->scalar('apellidos')
         ->maxLength('apellidos', 100)
         ->requirePresence('apellidos', 'create')
-        ->notEmpty('apellidos');
+        /*->notEmpty('apellidos')*/;
 
         return $validator;
     }
@@ -99,5 +104,60 @@ class AutoresTable extends Table
         $apellidos = $options['apellidos'];
         return $query->where(['nombre LIKE' => "%$nombre%",
                             'apellidos LIKE' => "%$apellidos%"]);
+    }
+    
+    public function findLibrosNoAsignados(Query $query, array $options)
+    {
+        // return id, titulo
+        $autor_id = $options['id'];
+        $libros_asignados = array();
+        $autor = $query->where(['id' => $autor_id])
+            ->contain('Libros')
+            ->first();
+        Log::write('debug', $autor);
+        foreach ($autor->libros as $l)
+        return $autor;
+    }
+
+    public function beforeSave($event, $entity, $options) {
+        $apenom = "";
+        if (trim($entity->apellidos) == '') {
+            $apenom = trim($entity->nombre);
+        } else {
+            $apenom = trim($entity->apellidos);
+            if (trim($entity->nombre) != '') {
+                $apenom .= ', ' . trim($entity->nombre);
+            }
+        }
+        $entity->ape_nom = $apenom;
+    }
+
+    /**
+     * @return \Search\Manager
+     */
+    public function searchManager()
+    {
+        $searchManager = $this->behaviors()->Search->searchManager();
+        $searchManager
+            ->add('busca_nombre', 'Search.Like', [
+                'before' => true,
+                'after' => true,
+                'fieldMode' => 'OR',
+                'comparison' => 'LIKE',
+                'wildcardAny' => '*',
+                'wildcardOne' => '?',
+                'field' => ['nombre']
+            ])
+            ->add('busca_apellidos', 'Search.Like', [
+                'before' => true,
+                'after' => true,
+                'fieldMode' => 'OR',
+                'comparison' => 'LIKE',
+                'wildcardAny' => '*',
+                'wildcardOne' => '?',
+                'field' => ['apellidos']
+            ]);
+
+        return $searchManager;
     }
 }
